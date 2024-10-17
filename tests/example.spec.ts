d4c0a9a53d717dd7c4b37f77a86d42a8f94b3060
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2024-10-17 19:27:36 -0400

    Update test to not do multiple snapshots

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,5 @@
 import { expect, test, takeSnapshot } from '@chromatic-com/playwright';
 
-// TODO:
-// It's taking multiple screenshots for each test.
-// May need to disable the built-in playwright assertion
-// in favor of us explicitly using `takeSnapshot`? Or just
-// rely on the built-in functionality for non-interactable UI.
-
 test('renders a primary button', async ({ page }, testInfo) => {
   await page.goto('/iframe.html?args=&id=button--primary&viewMode=story');
 
@@ -13,7 +7,8 @@ test('renders a primary button', async ({ page }, testInfo) => {
 
   await page.waitForSelector('ds-button');
 
-  await takeSnapshot(page, testInfo);
+  // Call this when we want to test hovering, for example.
+  // await takeSnapshot(page, testInfo);
 });
 
 test('renders a secondary button', async ({ page }, testInfo) => {
@@ -23,7 +18,7 @@ test('renders a secondary button', async ({ page }, testInfo) => {
 
   await page.waitForSelector('ds-button');
 
-  await takeSnapshot(page, testInfo);
+  // await takeSnapshot(page, testInfo);
 });
 
 test('renders a primary small button', async ({ page }, testInfo) => {
@@ -33,7 +28,7 @@ test('renders a primary small button', async ({ page }, testInfo) => {
 
   await page.waitForSelector('ds-button');
 
-  await takeSnapshot(page, testInfo);
+  // await takeSnapshot(page, testInfo);
 });
 
 test('renders a secondary small button', async ({ page }, testInfo) => {
@@ -45,5 +40,5 @@ test('renders a secondary small button', async ({ page }, testInfo) => {
 
   await page.waitForSelector('ds-button');
 
-  await takeSnapshot(page, testInfo);
+  // await takeSnapshot(page, testInfo);
 });
