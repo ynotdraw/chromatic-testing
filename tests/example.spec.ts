b67052a953cde665ed5d04d4d09bb818d5aa73a8
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2024-10-17 19:05:21 -0400

    Upgrade storybook

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,11 @@
 import { expect, test, takeSnapshot } from '@chromatic-com/playwright';
 
+// TODO:
+// It's taking multiple screenshots for each test.
+// May need to disable the built-in playwright assertion
+// in favor of us explicitly using `takeSnapshot`? Or just
+// rely on the built-in functionality for non-interactable UI.
+
 test('renders a primary button', async ({ page }, testInfo) => {
   await page.goto('/iframe.html?args=&id=button--primary&viewMode=story');
 
